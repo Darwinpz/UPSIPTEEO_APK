cb9619c057931c39598100f357adcf99f56a8d8a
Darwinpz <dpilaloazea@gmail.com>
2024-09-26 17:38:58 -0500

    Eliminación de importaciones sin usar

--- app/src/main/java/com/dpilaloa/upsipteeo/ui/activities/AddUserActivity.java
+++ app/src/main/java/com/dpilaloa/upsipteeo/ui/activities/AddUserActivity.java
@@ -2,8 +2,6 @@ package com.dpilaloa.upsipteeo.ui.activities;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
